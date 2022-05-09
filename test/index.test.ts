aa9dc6c4834dd39a38a9c89bc199d21ee0fdb924
Don McCurdy <dm@donmccurdy.com>
2022-05-08 23:16:58 -0400

    Add Prettier

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,7 +3,7 @@ require('source-map-support').install();
 import * as test from 'tape';
 import { implemented } from '../';
 
-test('test', t => {
+test('test', (t) => {
 	t.equals(implemented, true, 'implement library');
 	t.end();
 });
