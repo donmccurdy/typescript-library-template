57c6af63cebe183c049b88fd075e249e9a857494
Don McCurdy <dm@donmccurdy.com>
2024-04-10 21:09:35 -0400

    fix(ci): Updates for ESLint v9

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+// @ts-check
+import prettier from 'eslint-config-prettier';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
+	prettier,
+	{
+		rules: {
+			'@typescript-eslint/no-use-before-define': 'off',
+			'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+			quotes: ['warn', 'single'],
+			'max-len': [
+				'warn',
+				{ code: 100, tabWidth: 4, ignoreUrls: true, ignorePattern: '^import|^export' },
+			],
+			'newline-per-chained-call': [2, { ignoreChainWithDepth: 3 }],
+		},
+	},
+);
